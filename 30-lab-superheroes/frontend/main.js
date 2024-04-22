e55fea95c68cf3ebe8395203786488b578283e35
Emmanuel Toledo Castro <emmanueltoledo1082@gmail.com>
2024-04-21 20:25:27 -0600

    Practica actualizada

--- 30-lab-superheroes/frontend/main.js
+++ 30-lab-superheroes/frontend/main.js
@@ -14,37 +14,54 @@ const initializeComponents = () => {
   });
 
   document.querySelectorAll(".menu-item").forEach((menuItem) => {
-
     menuItem.addEventListener("click", (e) => {
       // e.target = Instancia de la clase Element, es decir, nuestro menuItem
       const publisher = e.target.getAttribute("att-publisher");
       console.log(publisher);
     });
+  });
+};
 
+/**
+ * Función para agregar a partir de un arreglo de 'Heroes', en la vista HTML.
+ */
+const addHeroesToPage = (heroes) => {
+  heroes.forEach((heroe) => {
+    // Creamos tarjeta de heroe
+    const heroeCard = document.createElement("div");
+    heroeCard.classList.add("superhero-card");
+
+    // Agregamos contenido a tarjeta de heroe
+    heroeCard.innerHTML = `
+    <img src="./imgs/${ heroe.img }" alt="${ heroe.superhero }">
+    <div class="info">
+        <p>Nombre:<span>${ heroe.superhero }</span></p>
+        <p>Alter ego:<span>XXXXXX</span></p>
+        <p>Actores:<span>XXXXXX</span></p>
+        <p>Publicador:<span>XXXXXX</span></p>
+        <p>Primer aparición:<span>XXXXXX</span></p>
+    </div>`;
+
+    document.querySelector("#heroes-container").append(heroeCard);
   });
 };
 
 // fetch es para consultar o consumir servicios web
 // ./ = directorio donde me encuentro
 // ../ = salir al directorio padre
-fetch('./../backend/db.json') // Promise<Fetch>
-  .then( (response) => { // Promise<Fetch>
-    console.log(response);
-    return response.json(); // Promise<J>
-  })
-  .then( (data) => { // Promise<J>
-    console.log(data);
-  })
-  .catch()
-  .finally();
+fetch("./../backend/db.json") // Retorna: Promise<Fetch>
+  // Recibe resolve de Fetch
+  .then((response) => response.json()) // Retorna: Promise<JSON>
+  // Recibe resolve de JSON
+  .then((data) => addHeroesToPage(data.heroes));
 
 initializeComponents();
 
-// Promesa 
-  // --> Response
+// Promesa
+// --> Response
 
-// Promesa 
-  // --> string
+// Promesa
+// --> string
 
-// Promesa 
-  // --> {  } | [] | { heroes: [] }
+// Promesa
+// --> {  } | [] | { heroes: [] }
